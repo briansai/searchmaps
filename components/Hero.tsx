1a7056b82737b860f8776b79d1c99c3f9465ce24
Brian Sai <brianwsai@gmail.com>
2023-11-07 19:05:25 -0800

    Input text content into Hero component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,16 @@
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <div className="hero w-full h-screen">
+      <div className="flex pt-52 px-56">
+        <div className="">
+          <div className="font-bold text-5xl">SearchMaps</div>
+          <div className="text-xl pt-5">
+            Search through your saved locations with ease and confidence
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
