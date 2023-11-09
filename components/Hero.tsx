8ff2b39da390443c60de83259927d79d54a7d11a
Brian Sai <brianwsai@gmail.com>
2023-11-09 11:13:28 -0800

    Moved Hero text lower

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,8 +3,8 @@ import { Login } from '@/components';
 const Hero = () => {
   return (
     <div className="hero w-full h-screen">
-      <div className="flex pt-52 px-56 justify-between items-center">
-        <div className="hero-text w-2/5">
+      <div className="flex pt-52 px-56 justify-between">
+        <div className="hero-text w-2/5 mt-14">
           <div className="font-bold text-5xl">SearchMaps</div>
           <div className="text-xl pt-5">
             Take control of your locations and search through them seamlessly.
